17bfd00f3961ea49de93befcb0b55742ee157329
Leon <leon.karho.chung@gmail.com>
2024-03-11 16:02:41 -0400

    react router setup and array of routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./global.scss";
+import Dashboard from "./pages/Dashboard.tsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+	{ path: "/dashboard", element: <Dashboard /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
