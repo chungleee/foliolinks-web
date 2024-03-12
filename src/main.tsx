90cc00f56bac34eddebbb4a228dfd1cf29e6299f
Leon <leon.karho.chung@gmail.com>
2024-03-12 16:03:45 -0400

    update login page path

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./global.scss";
-import Dashboard from "./pages/Dashboard.tsx";
+import Dashboard from "./pages/Dashboard/Dashboard.tsx";
+import Login from "./pages/Auth/Login/Login.tsx";
 
 const router = createBrowserRouter([
 	{
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
 		element: <App />,
 	},
 	{ path: "/dashboard", element: <Dashboard /> },
+	{ path: "/login", element: <Login /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
