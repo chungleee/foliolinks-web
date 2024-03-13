2316fc837079200488cddec5a4e53b536c66bdd9
Leon <leon.karho.chung@gmail.com>
2024-03-13 12:27:25 -0400

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./global.scss";
-import Dashboard from "./pages/Dashboard/Dashboard.tsx";
+import AddLinks from "./pages/Dashboard/AddLinks/AddLinks.tsx";
 import Login from "./pages/Auth/Login/Login.tsx";
 import Register from "./pages/Auth/Register/Register.tsx";
+import Profile from "./pages/Dashboard/Profile/Profile.tsx";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
 	},
-	{ path: "/dashboard", element: <Dashboard /> },
+	{ path: "/dashboard", element: <AddLinks /> },
+	{ path: "/dashboard/profile", element: <Profile /> },
 	{ path: "/login", element: <Login /> },
 	{ path: "/register", element: <Register /> },
 ]);
