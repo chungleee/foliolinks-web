bf91018fdd5d7e4a68bac0bd2217103afd1b2386
Leon <leon.karho.chung@gmail.com>
2024-03-20 13:46:16 -0400

    navigate to dashboard is authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "./hooks";
 
 function App() {
-	useAuth();
-
-	return (
-		<>
-			<h1>hello world</h1>
-		</>
-	);
+	const isAuth = useAuth();
+	const navigate = useNavigate();
+	if (isAuth) {
+		navigate("/dashboard");
+	}
+	// return (
+	// 	<>
+	// 		<h1>hello world</h1>
+	// 	</>
+	// );
 }
 
 export default App;
