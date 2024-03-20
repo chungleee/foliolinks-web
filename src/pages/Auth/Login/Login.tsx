4a19fb653ef06b750cbfb9f7a8b393065789066a
Leon <leon.karho.chung@gmail.com>
2024-03-20 16:57:15 -0400

    pls work this time????

--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -2,14 +2,14 @@ import styles from "./Login.module.scss";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FunctionComponent, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { TLoginFormInputs, loginSchema } from "../model";
 import TextField from "../../../components/common/TextField/TextField";
 import Button from "../../../components/common/Button/Button";
 import AuthLayout from "../AuthLayout";
 
 const Login: FunctionComponent = () => {
-	// const navigate = useNavigate();
+	const navigate = useNavigate();
 	const [isPending, setIsPending] = useState(false);
 
 	const {
@@ -33,10 +33,10 @@ const Login: FunctionComponent = () => {
 			});
 
 			const json = await result.json();
-			const { access_token } = json;
+			const { access_token } = await json;
 
-			localStorage.setItem("foliolinks_access_token", access_token);
-			// navigate("/dashboard");
+			await localStorage.setItem("foliolinks_access_token", access_token);
+			await navigate("/dashboard");
 		} catch (error) {
 			console.log(error);
 			setIsPending(false);
