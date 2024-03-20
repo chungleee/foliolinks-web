95bd38d97c0c00e778f5cc920ca1a92ef2ef0874
Leon <leon.karho.chung@gmail.com>
2024-03-20 13:51:05 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import { useAuth } from "./hooks";
 function App() {
 	const isAuth = useAuth();
 	const navigate = useNavigate();
+
 	if (isAuth) {
 		navigate("/dashboard");
 	}
-	// return (
-	// 	<>
-	// 		<h1>hello world</h1>
-	// 	</>
-	// );
+
+	return (
+		<>
+			<h1>hello world</h1>
+		</>
+	);
 }
 
 export default App;
