135758a43e92464fd5002a96f881e32d8417cfe3
chungleee <leon.karho.chung@gmail.com>
2024-03-21 15:15:51 -0400

    import prod env

--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -23,14 +23,15 @@ const Login: FunctionComponent = () => {
 	const onLoginSubmit = async (data: TLoginFormInputs) => {
 		setIsPending(true);
 		try {
-			let url = "https://foliolinks-api.leonchung.ca";
-
-			const result = await fetch(`${url}/api/users/auth/login`, {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify(data),
-				credentials: "include",
-			});
+			const result = await fetch(
+				`${import.meta.env.VITE_PROD_URL}/api/users/auth/login`,
+				{
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(data),
+					credentials: "include",
+				}
+			);
 
 			const json = await result.json();
 			const { access_token } = await json;
