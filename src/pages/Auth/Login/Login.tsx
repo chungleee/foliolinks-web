435fdf3942e00dfd149f241b39d80e59698e76dc
Leon <leon.karho.chung@gmail.com>
2024-03-22 13:31:19 -0400

    use env vars

--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -23,15 +23,16 @@ const Login: FunctionComponent = () => {
 	const onLoginSubmit = async (data: TLoginFormInputs) => {
 		setIsPending(true);
 		try {
-			const result = await fetch(
-				`${import.meta.env.VITE_PROD_URL}/api/users/auth/login`,
-				{
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-					body: JSON.stringify(data),
-					credentials: "include",
-				}
-			);
+			let url = import.meta.env.DEV
+				? import.meta.env.VITE_DEV_API
+				: import.meta.env.VITE_PROD_URL;
+
+			const result = await fetch(`${url}/api/users/auth/login`, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(data),
+				credentials: "include",
+			});
 
 			const json = await result.json();
 			const { access_token } = await json;
