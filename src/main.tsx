5e5147b1a43289656a0c33f85d5483947e3a2fb1
Leon <leon.karho.chung@gmail.com>
2024-10-23 16:13:47 -0400

    Initial Setup For Multi Step Registration
    - Implement children routing
    - Create user info form validation type
    - Move code from Register.tsx into CreateAccount as Register will serve as parent route component

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,8 @@ import ProtectedRoute from "./pages/ProtectedRoute.tsx";
 import { UserProvider } from "./contexts/UserContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ProjectsProvider } from "./contexts/ProjectsContext.tsx";
+import CreateUserAccount from "./pages/Auth/CreateAccount/CreateAccount.tsx";
+import CreateUserInfo from "./pages/Auth/CreateUserInfo/CreateUserInfo.tsx";
 
 const queryClient = new QueryClient();
 
@@ -46,7 +48,15 @@ const router = createBrowserRouter([
 		),
 	},
 	{ path: "/login", element: <Login /> },
-	{ path: "/register", element: <Register /> },
+	// { path: "/register", element: <Register /> },
+	{
+		path: "/register",
+		element: <Register />,
+		children: [
+			{ path: "", element: <CreateUserAccount /> },
+			{ path: "userinfo", element: <CreateUserInfo /> },
+		],
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
---
+++ src/pages/Auth/CreateUserInfo/CreateUserInfo.tsx
@@ -0,0 +1,9 @@
+const CreateUserInfo = () => {
+	return (
+		<>
+			<h1>create user info</h1>
+		</>
+	);
+};
+
+export default CreateUserInfo;
