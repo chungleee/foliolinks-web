cd8715fd6faf9b1cf52e8572c6f3973ce85897ca
Leon <leon.karho.chung@gmail.com>
2025-01-06 14:08:27 -0500

    Spacing & Color
    - copy to clipboard feature placeholder

--- src/pages/Dashboard/Preview/Preview.tsx
+++ src/pages/Dashboard/Preview/Preview.tsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 import styles from "./Preview.module.scss";
 
-import DashboardLayout from "../DashboardLayout";
 import ProjectLink from "../../../components/common/ProjectLink/ProjectLink";
 
 import { UserContext } from "../../../contexts/UserContext";
 import { ProjectsContext } from "../../../contexts/ProjectsContext";
+import { Button } from "../../../components/common/Button/Button";
 
 const Preview = () => {
 	const { userProfile } = useContext(UserContext);
@@ -14,30 +15,39 @@ const Preview = () => {
 
 	const { firstName, lastName, email } = userProfile || {};
 
+	const handleCopyToClipboard = async () => {
+		await navigator.clipboard.writeText("testcopytoclipboard.com");
+	};
+
 	if (isProjectsLoading) return <h1>Loading...</h1>;
 
 	return (
-		<DashboardLayout>
-			<div className={styles.preview}>
-				<section className={styles.user_bio}>
-					<div className={styles.user_bio_avatar}>
-						<img alt='avatar' />
-					</div>
-					<div className={styles.user_bio_description}>
-						<h4>{`${email}`}</h4>
-						<h1>{`${firstName} ${lastName}`}</h1>
-					</div>
-				</section>
+		<div className={styles.preview}>
+			<nav className={styles.preview_nav}>
+				<Button variant='secondary'>
+					<Link to='/'>Return to Dashboard</Link>
+				</Button>
+				<Button onClick={handleCopyToClipboard}>Share Link</Button>
+			</nav>
 
-				<section className={styles.user_projects}>
-					<ul>
-						{projects?.map((project) => {
-							return <ProjectLink key={project.id} project={project} />;
-						})}
-					</ul>
-				</section>
-			</div>
-		</DashboardLayout>
+			<section className={styles.user_bio}>
+				<div className={styles.user_bio_avatar}>
+					<img alt='avatar' />
+				</div>
+				<div className={styles.user_bio_description}>
+					<h4>{`${email}`}</h4>
+					<h1>{`${firstName} ${lastName}`}</h1>
+				</div>
+			</section>
+
+			<section className={styles.user_projects}>
+				<ul>
+					{projects?.map((project) => {
+						return <ProjectLink key={project.id} project={project} />;
+					})}
+				</ul>
+			</section>
+		</div>
 	);
 };
 
