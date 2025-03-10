d653dd21d0b2d52c9fa7dd9f506358746002c972
Leon <leon.karho.chung@gmail.com>
2025-03-10 16:25:42 -0400

    Settings page
    - Styling
    - Section displaying api key and generate & revoke buttons

--- src/pages/Dashboard/Settings/Settings.tsx
+++ src/pages/Dashboard/Settings/Settings.tsx
@@ -1,7 +1,16 @@
 import styles from "./Settings.module.scss";
 import DashboardLayout from "../DashboardLayout";
+import TextField from "../../../components/common/TextField/TextField";
+import { Button } from "../../../components/common/Button/Button";
 
 const Settings = () => {
+	const handleGenerateApiKey = () => {
+		console.log("generating api key");
+	};
+
+	const handleRevokeApiKey = () => {
+		console.log("revoking api key");
+	};
 	return (
 		<DashboardLayout>
 			<div className={styles.settings}>
@@ -11,6 +20,14 @@ const Settings = () => {
 						account deletion.
 					</p>
 					<h2>Account Settings</h2>
+				</section>
+
+				<section className={styles.settings__main}>
+					<TextField label='API key' />
+					<div className={styles.settings__main_apiBtns}>
+						<Button onClick={handleGenerateApiKey}>Generate</Button>
+						<Button onClick={handleRevokeApiKey}>Revoke</Button>
+					</div>
 				</section>
 			</div>
 		</DashboardLayout>
